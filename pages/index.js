12b368cdd97e3cce40e1523c2fb48da27a6bca9f
yxbudgqjdqdxq <93042363+yxbudgqjdqdxq@users.noreply.github.com>
2025-09-24 00:50:45 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
-import { useRouter } from 'next/router'
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
-  const router = useRouter()
+  const [entered, setEntered] = useState(false);
 
   return (
-    <div className="landing">
-      <h1 onClick={() => router.push('/menu')} className="landing-button">
-        I Missed You Bubu
-      </h1>
+    <div className="main-container">
+      {!entered ? (
+        <button className="big-button" onClick={() => setEntered(true)}>
+          I Missed You Bubu
+        </button>
+      ) : (
+        <div className="button-container">
+          <Link href="/mood">
+            <button className="sexy-button">Feeling Moody Today?</button>
+          </Link>
+          <Link href="/chat">
+            <button className="sexy-button">Daily Affirmations</button>
+          </Link>
+        </div>
+      )}
     </div>
-  )
+  );
 }
